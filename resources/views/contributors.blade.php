ff32f59f8c3e2400d546316dc31b0cfa3bb814ed
BuildTools <unconfigured@null.spigotmc.org>
2019-05-31 18:46:34 -0700

    changing to laravel

--- resources/views/contributors.blade.php
+++ resources/views/contributors.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Connect4Browser
+                    Contributors
                 </div>
 
                 <div class="links">
